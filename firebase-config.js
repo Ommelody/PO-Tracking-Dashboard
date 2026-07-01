06218533e6ada6faf280610d5e1aa88b07d48dac
SAWARiN <137431083+Ommelody@users.noreply.github.com>
2026-07-01 10:40:07 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,10 +2,10 @@
    ใส่ค่าคอนฟิกของโปรเจกต์ Firebase ของคุณตรงนี้ (ดูวิธีหาได้ใน README.md หัวข้อ "ตั้งค่า Firebase")
    ตราบใดที่ยังเป็นค่า YOUR_... ระบบจะทำงานแบบออฟไลน์ (ใช้ข้อมูลตั้งต้นที่ฝังมาในไฟล์) โดยอัตโนมัติ */
 window.FIREBASE_CONFIG = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSy...",
+  authDomain: "po-tracking.firebaseapp.com",
+  projectId: "po-tracking",
+  storageBucket: "po-tracking.appspot.com",
+  messagingSenderId: "1234567890",
+  appId: "1:1234567890:web:abc123"
 };
